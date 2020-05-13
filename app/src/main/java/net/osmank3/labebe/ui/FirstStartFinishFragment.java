f583706ab8c11d13b64ecc07cdc43cfc239d6f54
osmank3 <osmank3@gmail.com>
2020-05-13 15:59:32 +0300

    FirstStartFinishFragment prepared

--- app/src/main/java/net/osmank3/labebe/ui/FirstStartFinishFragment.java
+++ app/src/main/java/net/osmank3/labebe/ui/FirstStartFinishFragment.java
@@ -8,66 +8,48 @@
 
 package net.osmank3.labebe.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import net.osmank3.labebe.MainActivity;
 import net.osmank3.labebe.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstStartFinishFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FirstStartFinishFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FirstStartFinishFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstStartFinishFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstStartFinishFragment newInstance(String param1, String param2) {
-        FirstStartFinishFragment fragment = new FirstStartFinishFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    View root;
+    private Button btnFirstStartFinish;
+    private SharedPreferences preferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_start_finish, container, false);
+        root = inflater.inflate(R.layout.fragment_first_start_finish, container, false);
+
+        initComponents();
+        registerEventHandlers();
+
+        return root;
+    }
+
+    private void initComponents() {
+        btnFirstStartFinish = root.findViewById(R.id.btnFirstStartFinish);
+        preferences = getActivity().getSharedPreferences(getResources().getString(R.string.app_name), Context.MODE_PRIVATE);
+    }
+
+    private void registerEventHandlers() {
+        btnFirstStartFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferences.edit().putBoolean("isFirstStart", false).apply();
+                MainActivity.navController.navigate(R.id.action_firstStartFinish_to_home);
+            }
+        });
     }
 }
